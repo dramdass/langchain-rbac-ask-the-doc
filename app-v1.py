68c997070e19c49c9ed33aa9aae302a681940fcd
dramdass <dramdass@gmail.com>
2023-12-09 19:56:08 -0800

    Change UI and retriever to support data classification
    Ask user to upload file with classification, specify claims in UI. Check claim with data classification and use ensemble retriever to equally weight sources

--- app-v1.py
+++ app-v1.py
@@ -5,9 +5,35 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
-def generate_response(uploaded_file, openai_api_key, query_text):
+def generate_response(classifications_files_dict, openai_api_key, query_text, user_claims):
     # Load document if file is uploaded
-    if uploaded_file is not None:
+    retrievers = []
+    for classification, file in classifications_files_dict:
+        if authorized(user_claims, classification):
+            retrievers.append(build_retriever(file, openai_api_key)
+
+    if retrievers is None:
+        return "No files authorized for current user"
+        
+    weights = [1/len(retrievers)] * len(retrievers)
+    # initialize the ensemble retriever
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=retreivers, weights=weights
+    )
+    # Create QA chain
+                          
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=ensemble_retriever)
+    return qa.run(query_text)
+
+def authorized(claims, classification):
+    # todo (dramdass)
+    for claim in claims:
+        if claim == classification:
+            return true
+    return false
+
+def build_retriever(uploaded_file, openai_api_key):
+    f uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
     # Split documents into chunks
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -18,29 +44,38 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     db = Chroma.from_documents(texts, embeddings)
     # Create retriever interface
     retriever = db.as_retriever()
-    # Create QA chain
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-    return qa.run(query_text)
+    return retriever
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-st.title('🦜🔗 Ask the Doc App')
+st.title('🦜🔗 RBAC - Ask the Doc App')
 
 # File upload
 internal_file = st.file_uploader('Upload an internal document', type='pdf')
 restricted_file = st.file_uploader('Upload a restricted document', type='pdf')
 confidential_file = st.file_uploader('Upload a confidential document', type='pdf')
+
+uploaded_files = {
+    'internal': internal_file,
+    'restricted': restricted_file,
+    'confidential': confidential_file
+}
+
+user_claims_string = st.text_input('Enter your claims separated by commas:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+user_claims = user_claims_string.split(',')
+
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
+disabled = not (uploaded_file and user_claims and query_text)
 # Form input and query
 result = []
 with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=disable)
+    submitted = st.form_submit_button('Submit', disabled=disabled)
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
+            response = generate_response(uploaded_files, openai_api_key, query_text)
             result.append(response)
             del openai_api_key
 
